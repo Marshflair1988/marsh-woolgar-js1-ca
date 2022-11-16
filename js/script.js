ceefcb85cd90ad3ce9df675213c80b3fb2daf4fb
Marshflair1988 <104839692+Marshflair1988@users.noreply.github.com>
2022-11-16 21:48:51 +0100

    java update

--- js/script.js
+++ js/script.js
@@ -11,7 +11,25 @@ async function callApi() {
 
         const results = await response.json();
 
-        console.log(results);
+        const name = results.name;
+        const Locations = results.state;
+        const type = results.brewery_type;
+
+        resultsContainer.innerHTML = "";
+
+        for(let i = 0; i < results.length; i++) {
+            console.log(results[i].name)
+            if(i === 12) {
+                break;
+            }
+
+            resultsContainer.innerHTML += `<div class="result">
+            <span><li>Name: ${results[i].name}</li></span>
+            <span><li>Location: ${results[i].state}</li></span>
+            <span><li>Type: ${results[i].brewery_type}</li></span>
+            </div>`;
+        }
+        
 
     } catch (error) {
         console.log(error);
@@ -20,4 +38,3 @@ async function callApi() {
 }
 
 callApi();
-
